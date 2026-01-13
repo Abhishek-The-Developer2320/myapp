776c92461b9c3d67f34fca2d4da559d8f3f077fc
Abhishek-The-Developer2320 <159622033+Abhishek-The-Developer2320@users.noreply.github.com>
2026-01-13 16:21:48 +0530

    Change

--- myproject/users/__init__.py
+++ myproject/users/__init__.py
@@ -1,10 +1,5 @@
 from flask import Blueprint
 
-# Creates a Blueprint object named 'users'.
-# The 'template_folder' tells this blueprint where to find its HTML files.
 users_bp = Blueprint('users', __name__, template_folder='../templates')
-
-# This import is placed at the bottom to avoid circular dependency issues.
-# It connects the routes defined in routes.py to this blueprint.
 from . import views
 
