2939fcd13a543512f08b0aa56b3d098a2c52dc4a
Abhishek-The-Developer2320 <159622033+Abhishek-The-Developer2320@users.noreply.github.com>
2026-01-13 16:18:33 +0530

    change

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Create instances of the database and migration engine.
-# These will be initialized with the Flask app in run.py.
 db = SQLAlchemy()
 migrate = Migrate()
 
